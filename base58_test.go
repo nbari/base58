90a1376b8862ec945d9df0d9ae8241a91d99df07
nbari <nbari@tequila.io>
2017-08-17 10:42:02 +0200

    modified:   base58_test.go

--- base58_test.go
+++ base58_test.go
@@ -4,13 +4,51 @@ import (
 	"testing"
 )
 
-func TestEncode(t *testing.T) {
+var testTable = []struct {
+	in  uint64
+	out string
+}{
+	{0, "1"},
+	{1, "2"},
+	{32, "Z"},
+	{58, "21"},
+	{59, "22"},
+	{64, "27"},
+	{1024, "Jf"},
+	{4096, "2Dd"},
+	{8192, "3SF"},
+	{16384, "5sV"},
+	{65536, "LUw"},
+	{4294967296, "7YXq9H"},
+	{18446744073709551615, "jpXCZedGfVQ"},
+}
+
+// how to make this fast
+func TestEncodeDecode(t *testing.T) {
 	var i uint64
-	for i = 0; i <= (1<<64 - 1); i++ {
+	for i = 0; i <= (1 << 16); i++ {
 		b58 := Encode(i)
 		num := Decode(b58)
 		if num != i {
 			t.Fatalf("Expecting %d for %s", i, b58)
+		}
+	}
+}
+
+func TestEncode(t *testing.T) {
+	for _, tt := range testTable {
+		b58 := Encode(tt.in)
+		if b58 != tt.out {
+			t.Fatalf("Expecting %v for %s", b58, tt.out)
+		}
+	}
+}
+
+func TestDecode(t *testing.T) {
+	for _, tt := range testTable {
+		num := Decode(tt.out)
+		if num != tt.in {
+			t.Fatalf("Expecting %d for %d", tt.in, num)
 		}
 	}
 }
